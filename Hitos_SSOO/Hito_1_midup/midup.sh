26b15117b58fa1d277ed093ca36e4ba255a69715
Maelson1 <libra-10-12@hotmail.com>
2025-10-18 18:59:40 +0200

    midup actualizacion

--- Hitos_SSOO/Hito_1_midup/midup.sh
+++ Hitos_SSOO/Hito_1_midup/midup.sh
@@ -6,6 +6,9 @@ precision=""
 bytes=""
 directorios=()
 
+#Definimos el tabulador para usarlo en la impresión por consola
+TAB=$'\t'
+
 bucle_recursivo(){ #Iniciamos un bucle while, el cual parseará los argumentos introducidos mediante la ejecución 
 while [ $# -gt 0 ]; do #El bucle leerá la cantidad de argumentos introducidos mediante $# y lo comparará con -gt (mayor que) 0, esto quiere decir que cuando la variable sea 0, el bucle se detiene
     recursiva="$1"
@@ -38,10 +41,42 @@ echo "Nivel de precisión: $precision"
 echo "Tamaño mínimo en bytes: $bytes"
 echo "Rutas a procesar: ${directorios[*]}"
 
+for dir in "${directorios[@]}"; do
+    if [ -d "$dir"]; then
+        for archivo in "$dir"/*; do
+            if [ -f "$archivo" ]; then
+                nombre=$(basename "$archivo")
+                ruta=$(realpath "$archivo")
+                tamano=$(stat -c%s "$archivo")
+                echo -e "${nombre}${TAB}${ruta}${TAB}${tamano} bytes"
+            fi
+        done
+    else
+        echo "La ruta $dir no es un directorio válido."
+    fi
+done | sort -t$'\t' -k1,1
+
 #Vamos a establecer una serie de controles antes de que el programa pueda ejecutarse de manera correcta
 
 #control para profundidad
-if ! [[ "$profundidad" =~ ^[0-9]+$ ]]; then
+if [ -n "$profundidad" ] &&! [[ "$profundidad" =~ ^[0-9]+$ ]]; then
     echo "Lo siento, introduzca un valor válido para profundidad (-p) en el próximo intento."
     exit 1
+fi
+
+#control para precision
+if [ -n "$precision" ] &&! [[ "$precision" =~ ^[0-9]+$ ]]; then
+    echo "Lo siento, introduzca un valor válido para profundidad (-p) en el próximo intento."
+    exit 1
+fi
+
+#control para bytes
+if [ -n "$bytes" ] &&! [[ "$bytes" =~ ^[0-9]+$ ]]; then
+    echo "Lo siento, introduzca un valor válido para bytes (-s) en el próximo intento."
+    exit 1
+fi
+#control para rutas
+if [${directorios[@]} -eq 0]; then
+    then $directorios=("./")
+    echo "No se ha detectado ninguna ruta, se usará la ruta actual por defecto."
 fi
